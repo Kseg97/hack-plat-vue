b379aaf03086d663fcf0268dc3447a28f139febd
sterang <hamil@unicauca.edu.co>
2020-08-03 00:50:00 -0500

    cargando news

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,83 +1,86 @@
 <template>
-  <div class="col-md-10 container" >
-      <h3 class="text-center">Events</h3>
-      
-         <div v-for="event in events" v-bind:key="event">
-            
-            <div class="card mb-3 mt-3 shadow-sm ">
-                <div class="row no-gutters">
-                    <h4 class= "card-title">{{event.name}}</h4>
-                    <p class="card-text text-muted"> {{event.description}}</p>
-                    <br/>
-                    <p class="card-text"> Source: {{event.source}}</p>
-                    <br/>
-                    <p class="card-text"> Clossing date: {{event.clossing_date}}</p>
-                    <br/>
-                    <p><a href="#" class="card-link text-right">Inscribirse</a></p>
-                </div>
-            </div>
+  <div style="background-image: url(img/loginScreen.png);">
+    <div class="container">
+      <b-carousel
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
+        indicators
+        img-width="1024"
+        img-height="780"
+      >
+        <b-carousel-slide
+          caption="First Slide"
+          img-src="https://cdn.hswstatic.com/gif/becoming-doctor.jpg"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Second Slide"
+          img-src="https://www.unodc.org/images/dohadeclaration/page-media/news/2019/11/hacking-for-justice--impressive-young-coders-compete-at-world-bank-global-hackathon_html/Webstory-cover.jpg"
+        ></b-carousel-slide>
+        <b-carousel-slide
+          caption="Third Slide"
+          img-src="https://tec.mx/sites/default/files/styles/header_full/public/2020-03/facts-coronavirus-covid-19-tec-de-monterrey.jpg?itok=KMQwCavT"
+        ></b-carousel-slide>
+      </b-carousel>
+      <div class="mt-4">
+        <h4 style="color:white">
+          <strong>News and More</strong>
+        </h4>
+      </div>
+      <div class="mt-4 container">
+        <div v-for="newevent in news" :key="newevent.name">
+          <b-card
+            :img-src="newevent.ima"
+            img-alt="Card image"
+            img-left
+            class="mb-3"
+            :title="newevent.name"
+            :sub-title="newevent.source"
+          >
+            <b-card-text>{{newevent.description}}</b-card-text>
+            <b-button href="#" variant="primary">See more</b-button>
+          </b-card>
         </div>
-      
-    
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 //import { newsCollection } from "../firebase";
-import { ToggleButton } from "vue-js-toggle-button";
-import Vue from "vue";
-
-Vue.component("ToggleButton", ToggleButton);
-
+//import { ToggleButton } from "vue-js-toggle-button";
+//import Vue from "vue";
+//Vue.component("ToggleButton", ToggleButton);
 export default {
   data() {
     return {
-      events:this.loadEvents()
+      news: this.loadNews(),
     };
   },
   methods: {
-    loadEvents() {
-     // const news = newsCollection.get().doc.map(doc => doc.data());
-    const  events = [
-          {
-              name: "Hackaton por la vida",
-              description: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took ....",
-              source: "Hackaton Fundation",
-              clossing_date: "15-09-2020"
-          },
-             {
-              name: "NasaApps",
-              description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ...",
-              source: "Nasa US",
-              clossing_date: "20-09-2020"
-          },
-          {
-              name: "Google Robotics",
-              description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney Colle...",
-              source: "Google Inc.",
-              clossing_date: "31-11-2020"
-          },
-          {
-              name: "Hackatravel 2021",
-              description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney Colle...",
-              source: "Google Inc.",
-              clossing_date: "1-04-2021"
-          },
-          {
-              name: "Mustache Hackaton",
-              description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney Colle...",
-              source: "Dave  Inc.",
-              clossing_date: "30-02-2020"
-          },
-          {
-              name: "National Y4PT Colombia",
-              description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney Colle...",
-              source: "Bogota",
-              clossing_date: "15-09-2020"
-          },
+    loadNews() {
+      // const news = newsCollection.get().doc.map(doc => doc.data());
+      const news = [
+        {
+          name: "Ganadores de la Hackaton x",
+          description:
+            "El equipo AESS Unicauca se llevo el primero puesto de la hackaton x con un proyecto muy innovador para hacer mas facil la interaccion entre usuarios en las Hackatones............",
+          source: "Hackaton Fundation",
+          ima:
+            "https://choosedo.org/wp-content/uploads/2019/01/why-becomeresize-350x350.jpg",
+        },
+
+        {
+          name: "Ganadores de la Hackaton wwwwwww",
+          description:
+            "The team AESS Unicauca  puesto de la hackaton x con un proyecto muy innovador para hacer mas facil la interaccion entre usuarios en las Hackatones............",
+          source: "Hackaton Fundation",
+          ima:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSfbdyxJEoooblxagPuTcdH_0NYqu-PtFePrA&usqp=CAU",
+        },
       ];
-      console.log(events);
-      return events;
+      console.log(news);
+      return news;
     },
   },
 };
