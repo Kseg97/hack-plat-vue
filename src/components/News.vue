12f70e77f3f25023703bcd76c64b23c4eb05d31f
sterang <hamil@unicauca.edu.co>
2020-08-03 00:27:03 -0500

    cambiando estilo de noticias

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="col-md-10 container" >
-      <h3 class="text-center">News</h3>
-      
-        <div v-for="vnew in news" v-bind:key="vnew">
-            <div class="card mb-3 mt-3 shadow-sm ">
-                <div class="row no-gutters">
-                    <h4 class= "card-title">{{vnew.name}}</h4>
-                    <p class="card-text text-muted"> {{vnew.description}}</p>
-                    <p class="card-text"> Source: {{vnew.source}}</p>
-                </div>
-            </div>
+  <div style="background-image: url(img/loginScreen.png); height: 600px;">
+    <div class="col-md-9 container">
+      <h3 class="text-center" style="color:white">
+        <strong>News</strong>
+      </h3>
+      <div v-for="vnew in news" v-bind:key="vnew">
+        <div class="card mb-3 mt-3 shadow-sm" style="padding: 10px">
+          <div class="row no-gutters">
+            <h4 style="color:rgb(31 59 125)" class="card-title">
+              <strong>{{vnew.name}}</strong>
+            </h4>
+            <p class="card-text text-muted">{{vnew.description}}</p>
+            <p style="color:#525f7f" class="card-text">Source: {{vnew.source}}</p>
+          </div>
         </div>
-      
-    
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,30 +29,32 @@ Vue.component("ToggleButton", ToggleButton);
 export default {
   data() {
     return {
-      news:this.loadNews()
+      news: this.loadNews(),
     };
   },
   methods: {
     loadNews() {
-     // const news = newsCollection.get().doc.map(doc => doc.data());
-    const  news = [
-          {
-              name: "Ganadores de la Hackaton x",
-              description: "El equipo AESS Unicauca se llevo el primero puesto de la hackaton x con un proyecto muy innovador para hacer mas facil la interaccion entre usuarios en las Hackatones............",
-              source: "Hackaton Fundation"
-          },
-             {
-              name: "Ganadores de la Hackaton qqqq",
-              description: "El equipo AESS Unicauca se llevo el primero puesto de la hackaton x con un proyecto muy innovador para hacer mas facil la interaccion entre usuarios en las Hackatones............",
-              source: "Hackaton Fundation"
-          },
-             {
-              name: "Ganadores de la Hackaton wwwwwww",
-              description: "El equipo AESS Unicauca se llevo el primero puesto de la hackaton x con un proyecto muy innovador para hacer mas facil la interaccion entre usuarios en las Hackatones............",
-              source: "Hackaton Fundation"
-          }
+      // const news = newsCollection.get().doc.map(doc => doc.data());
+      const news = [
+        {
+          name: "Winners of the Hackathon 2020",
+          description:
+            "The AESS Unicauca team took first place in the hackathon with a very innovative project to make user interaction easier in Hackathons.",
+          source: "University of Cauca",
+        },
+        {
+          name: "Winners of the Hackathon 2019",
+          description:
+            "The AESS Unicauca team took first place in the hackathon with a very innovative project to make user interaction easier in Hackathons.",
+          source: "Hackathon Fundation",
+        },
+        {
+          name: "Winners of the Hackathon 2018",
+          description:
+            "The AESS Unicauca team took first place in the hackathon with a very innovative project to make user interaction easier in Hackathons.",
+          source: "Hackathon Fundation",
+        },
       ];
-      console.log(news);
       return news;
     },
   },
